9de0cf7581c22adbba881cb79d51d0309a3ea3dd
Elaine Soares <elaine.fabiola@gmail.com>
2025-07-30 15:05:51 -0300

    interface ICrudOperations
    Interface que define um contrato padrão para operações de
    Cadastro, Leitura, Atualização e Deleção (CRUD).
    @param <T> O tipo da entidade que será manipulada.

---
+++ src/ICrudOperations.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+/**
+ * Interface que define um contrato padrão para operações de
+ * Cadastro, Leitura, Atualização e Deleção (CRUD).
+ * @param <T> O tipo da entidade que será manipulada.
+ */
+public interface ICrudOperations<T> {
+
+    boolean cadastrar(T entidade);
+    T buscarPorId(Long id);
+    List<T> listarTodos();
+    boolean atualizar(T entidade);
+    boolean remover(Long id);
+}
