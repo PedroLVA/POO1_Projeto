4d8750df54d48d5a17a32b91d56052c47cb77f2b
Pedro Athayde <pedro_athayde@outlook.com>
2025-08-06 17:39:00 -0300

    chore: remove unused imports from main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,4 @@
-import exception.PessoaNaoEncontradaException;
-import model.Ator;
-import model.Diretor;
-import service.AtorService;
-import service.DiretorService;
-
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
 
     }
